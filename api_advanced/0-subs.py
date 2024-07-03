bd49bf0156de6b06150090bcb0501b50ba795c36
root <s.mushy1@alustudent.com>
2024-07-03 11:04:46 +0000

    made corrections in the first file

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,17 +2,19 @@
 """queries the Reddit API and returns the number of subscribers for a given subreddit"""
 
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API and returns the number of subscribers
     for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+if (type(subreddit) is not str):
+        return(0)
+
+    url_api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
     if json_obj.status_code != 404:
         dict_obj = json_obj.json()
         return dict_obj.get('data').get('subscribers')
     else:
-        return 0
+        return (0)
