ca951823643486bc2820173245b1986aa1d1b0b7
root <s.mushy1@alustudent.com>
2024-07-04 19:07:13 +0000

    task 00000

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
-
+"""Return number of subscribers for a given subreddit"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
+    """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "PostmanRuntime/7.35.0"}
-
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    
     try:
-        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return response.json().get("data").get("subscribers")
-
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
+        response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
+        
+        data = response.json()
+        
+        # Ensure 'data' key exists in the JSON response
+        if 'data' in data:
+            return data['data'].get('subscribers', 0)  # Default to 0 if 'subscribers' key is missing
+        else:
+            return 0
+    except requests.RequestException:
         return 0
