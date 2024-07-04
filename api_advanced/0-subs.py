80aac24f6d3ea574a3187c4de775aa04e60d4cc3
root <s.mushy1@alustudent.com>
2024-07-04 09:33:51 +0000

    changed the format of task one

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,16 +5,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of subscribers
-    for a given subreddit."""
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-if (type(subreddit) is not str):
-        return(0)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-    url_api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
-    if json_obj.status_code != 404:
-        dict_obj = json_obj.json()
-        return dict_obj.get('data').get('subscribers')
-    else:
-        return (0)
+    return sub_info.json().get("data").get("subscribers")
