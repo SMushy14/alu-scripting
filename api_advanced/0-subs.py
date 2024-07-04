825e6539d73793f1c349f1e0a9dad6697020d7c2
root <s.mushy1@alustudent.com>
2024-07-04 15:56:51 +0000

    abc

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
- function that queries the Reddit API
- and returns the number of subscribers
-"""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """initializate"""
-    if (type(subreddit) is not str):
-        return(0)
-    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(url_api, headers=headers)
-    if response.status_code is not 200:
-        return(0)
-    return(response.json().get("data").get("subscribers"))
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
+        return 0
