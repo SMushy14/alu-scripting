a9d6f78be6bcba69896e4320a0ad418233b95f5e
root <s.mushy1@alustudent.com>
2024-07-04 16:11:16 +0000

    task 000000000000000000000000000000

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,12 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "PostmanRuntime/7.35.0"}
 
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
+        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return response.json().get("data").get("subscribers")
 
     except Exception:
         return 0
