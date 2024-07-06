0eab08f93609882265581865c714a7248587781c
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2024-07-06 22:51:22 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return number of subscribers on a given subreddit"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit),
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "RedditSubscriberChecker/1.0 (by u/S_mushy14"
     }
