7017a3bc51ec09bfb7426f137ea471c13471d46e
Sharon Mushy <155962275+SMushy14@users.noreply.github.com>
2024-07-06 21:42:17 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
-        response.raise_for_status()  # Raises HTTPError for bad responses (4xx and 5xx)
-        
-        data = response.json()
-        
-        # Ensure 'data' key exists in the JSON response
-        if 'data' in data:
-            return data['data'].get('subscribers', 0)  # Default to 0 if 'subscribers' key is missing
-        else:
-            return 0
-    except requests.RequestException:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
         return 0
